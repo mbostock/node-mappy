f563f0b824888203925725d2613673e10453f23c
Mike Bostock <mbostock@gmail.com>
2011-05-23 14:17:46 -0700

    Checkpoint first working version.
    Some of the important things left to do:
    
      - Variable-size buffers when missing Content-Length.
      - Example of rendering dots on top of tiles.
      - Other projections? Make location-agnostic?
      - Full-scene antialiasing to eliminate tile seams.
      - Retry and throttling for tile-loading.
    
    But hey, it works!

---
+++ test/test-map.js
@@ -0,0 +1,29 @@
+var fs = require("fs"),
+    mappy = require("../"),
+    Canvas = require("../../node-canvas/lib/canvas"),
+    Image = Canvas.Image;
+
+var w = 1280,
+    h = 720;
+
+var view = mappy.view()
+    .size([w, h]);
+
+var image = mappy.image()
+    .view(view)
+    .url(mappy.url("http://{S}tile.cloudmade.com"
+    + "/1a1b06b230af4efdbb989ea99e9841af" // http://cloudmade.com/register
+    + "/998/256/{Z}/{X}/{Y}.png")
+    .hosts(["a.", "b.", "c.", ""]));
+
+var canvas = new Canvas(w, h),
+    context = canvas.getContext("2d");
+
+image.render(context, done);
+
+function done() {
+  var out = fs.createWriteStream(__dirname + "/test.png")
+  canvas.createPNGStream().on("data", function(chunk) {
+    out.write(chunk);
+  });
+}
