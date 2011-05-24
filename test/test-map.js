c8902b14a1afd3798a6ea788cf596c0dd0ac3642
Mike Bostock <mbostock@gmail.com>
2011-05-23 18:24:19 -0700

    Woo, animation!

--- test/test-map.js
+++ test/test-map.js
@@ -1,4 +1,5 @@
 var fs = require("fs"),
+    util = require("util"),
     mappy = require("../"),
     Canvas = require("../../node-canvas/lib/canvas"),
     Image = Canvas.Image;
@@ -8,27 +9,33 @@ var w = 1280,
 
 var view = mappy.view()
     .size([w, h])
-    .center([655, 1583, 12])
-    .zoomBy(-.1)
-    .angle(.1);
+    .center([655, 1583, 12]);
 
 var image = mappy.image()
     .view(view)
-    .zoom(13)
     .url(mappy.url("http://{S}tile.cloudmade.com"
     + "/1a1b06b230af4efdbb989ea99e9841af" // http://cloudmade.com/register
     + "/998/256/{Z}/{X}/{Y}.png")
     .hosts(["a.", "b.", "c.", ""]));
 
+var frame = 0,
+    frames = 1200;
+
 var canvas = new Canvas(w, h),
     context = canvas.getContext("2d");
 
 context.antialias = "subpixel";
-image.render(context, done);
 
-function done() {
-  var out = fs.createWriteStream("/tmp/test.png")
-  canvas.createPNGStream().on("data", function(chunk) {
-    out.write(chunk);
+loop();
+
+function loop() {
+  var i = ++frame;
+  if (i > frames) return;
+  image.render(context, function() {
+    util.log(__dirname + "/" + i + ".png");
+    fs.writeFile(__dirname + "/" + i + ".png", canvas.toBuffer(), function() {
+      view.zoomBy(-.002).rotateBy(-.001);
+      loop();
+    });
   });
 }
